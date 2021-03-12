1071b18d86a399fb33fd2b70cce1ff050a512b5c
Wanderson Ferreira <wand@hey.com>
2021-03-11 21:15:13 -0300

    compose exercise 2.1 arity repair

---
+++ src/sdff/dsl/arity.clj
@@ -0,0 +1,26 @@
+(ns sdff.dsl.arity)
+
+(def arity-table
+  (atom {}))
+
+
+(defn restrict-arity
+  [proc nargs]
+  (swap! arity-table assoc proc nargs)
+  proc)
+
+
+(defn naive-compute-arity
+  "This SO has some interesting discussion around computing arity in Clojure.
+https://stackoverflow.com/questions/1696693/clojure-how-to-find-out-the-arity-of-function-at-runtime"
+  [proc]
+  (let [m (first (.getDeclaredMethods (class proc)))
+        p (.getParameterTypes m)]
+    (alength p)))
+
+
+(defn get-arity
+  [proc]
+  (if-let [n (get proc @arity-table)]
+    n
+    (naive-compute-arity proc)))
--- src/sdff/dsl/compose.clj
+++ src/sdff/dsl/compose.clj
@@ -1,4 +1,6 @@
-(ns sdff.dsl.compose)
+(ns sdff.dsl.compose
+  (:require [clojure.test :as t]
+            [sdff.dsl.arity :as arity]))
 
 (defn compose
   [f g]
@@ -11,3 +13,39 @@
    'z)
   ;; => (foo (bar z))
   )
+
+
+;;; exercise 2.1. Arity repair
+;;; 1. check their components to make sure that the arities are compatible
+;;; 2. combination constructed checks that it is given the correct number of arguments when called
+;;; 3. combination advertises its arity correctly to get-arity
+
+(defn compose-ex2-1
+  [f g]
+  (let [n (arity/get-arity f)
+        m (arity/get-arity g)]
+    (assert (>= n 1))
+    (defn the-combination-compose [& args]
+      (assert (= (count args) m))
+      (f (apply g (take m args))))
+    (arity/restrict-arity the-combination-compose m)))
+
+
+(t/deftest verify-arity-constraints
+  (t/testing "correct expected case"
+    (t/is
+     (= ((compose-ex2-1 (fn [x] (list 'foo x))
+                        (fn [y] (list 'bar y)))
+         'z)
+        '(foo (bar z)))))
+
+  (t/testing "wrong number of args passed in runtime."
+    (t/is (thrown? AssertionError
+                   ((compose-ex2-1 (fn [x] (list 'foo x))
+                                   (fn [y] (list 'bar y)))
+                    'z 'x 'w))))
+
+  (t/testing "if arity of F is not at least 1, throw assertion error. We don't know how many items G is going to return."
+    (t/is (thrown? AssertionError
+                   ((compose-ex2-1 (fn [] (list 'foo))
+                                   (fn [y] (list 'bar y))) 'z)))))
