d509a17eca69adac2acc9cf3e1a6ad24614a82ff
Wanderson Ferreira <wand@hey.com>
2021-03-11 21:16:18 -0300

    fix namespace after moving arity to its own namespace

--- src/sdff/dsl/spread_combine.clj
+++ src/sdff/dsl/spread_combine.clj
@@ -1,32 +1,11 @@
-(ns sdff.dsl.spread-combine)
+(ns sdff.dsl.spread-combine
+  (:require [sdff.dsl.arity :as arity]))
 
 ;;; sneaky way to define the arity of the function
 
-(def arity-table
-  (atom {}))
-
-(defn restrict-arity
-  [proc nargs]
-  (swap! arity-table assoc proc nargs)
-  proc)
-
-(defn naive-compute-arity
-  "This SO has some interesting discussion around computing arity in Clojure.
-https://stackoverflow.com/questions/1696693/clojure-how-to-find-out-the-arity-of-function-at-runtime"
-  [proc]
-  (let [m (first (.getDeclaredMethods (class proc)))
-        p (.getParameterTypes m)]
-    (alength p)))
-
-(defn get-arity
-  [proc]
-  (if-let [n (get proc @arity-table)]
-    n
-    (naive-compute-arity proc)))
-
 (defn spread-combine-v1
   [h f g]
-  (let [n (get-arity f)]
+  (let [n (arity/get-arity f)]
     (fn [& args]
       (h (apply f (take n args))
          (apply g (drop n args))))))
@@ -35,26 +14,26 @@ https://stackoverflow.com/questions/1696693/clojure-how-to-find-out-the-arity-of
 (defn spread-combine-v2
   "Version using appropriate arity decorated."
   [h f g]
-  (let [n (get-arity f)
-        m (get-arity g)]
+  (let [n (arity/get-arity f)
+        m (arity/get-arity g)]
     (let [t (+ n m)]
       (defn the-combination [& args]
         (h (apply f (take n args))
            (apply g (drop n args))))
-      (restrict-arity the-combination t))))
+      (arity/restrict-arity the-combination t))))
 
 
 (defn spread-combine-v3
   "Version using assert to ensure we have the desired arity."
   [h f g]
-  (let [n (get-arity f)
-        m (get-arity g)]
+  (let [n (arity/get-arity f)
+        m (arity/get-arity g)]
     (let [t (+ n m)]
       (defn the-combination [& args]
         (assert (= (count args) t))
         (h (apply f (take n args))
            (apply g (drop n args))))
-      (restrict-arity the-combination t))))
+      (arity/restrict-arity the-combination t))))
 
 
 (comment
