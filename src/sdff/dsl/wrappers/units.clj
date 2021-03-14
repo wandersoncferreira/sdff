b3da21ce76a7eea8db45f99403e02e9b6c9e218b
Wanderson Ferreira <wand@hey.com>
2021-03-14 10:59:22 -0300

    unit conversion dsl

--- src/sdff/dsl/wrappers/units.clj
+++ src/sdff/dsl/wrappers/units.clj
@@ -6,13 +6,17 @@
 
 (def pi (* 4 (Math/atan 1)))
 
+(def expt (fn [v e] (Math/pow v e)))
+
+(def div /)
+
 (defn gas-law-volume
   [pressure temperature amount]
   (/ (* amount gas-constant temperature) pressure))
 
 (defn sphere-radius
   [volume]
-  (Math/pow (/ volume (* 4/3 pi)) 1/3))
+  (expt (/ volume (* 4/3 pi)) 1/3))
 
 (defn make-unit-conversion
   [f g]
@@ -67,3 +71,101 @@
                          ((lib.comb/compose celsius-to-kelvin fahrenheit-to-celsius) 68) 1)))
                       7.0496)))
 
+;;; specialization wrappers
+
+(defn unit:*
+  [u1 u2]
+  (make-unit-conversion (lib.comb/compose u2 u1)
+                        (lib.comb/compose (unit:invert u1)
+                                          (unit:invert u2))))
+
+(defn unit:div
+  [u1 u2]
+  (make-unit-conversion (fn [v1] (* v1 (/ (u1 1) (u2 1))))
+                        (fn [o1] (* o1 (/ ((unit:invert u1) 1)
+                                          ((unit:invert u2) 1))))))
+
+(defn unit:expt
+  "A bit bad bc my lib.comb/compose only works pair-wise."
+  [u n]
+  (cond
+    (= n 2) (unit:* u u)
+    (> n 2) (reduce
+             (fn [final-conv _u]
+               (unit:* final-conv _u))
+             (unit:* u u)
+             (repeat (- n 2) u))))
+
+(def ^:private lookup-table-for-unit-conversions
+  (atom {}))
+
+(defn register-unit-conversion!
+  [from to procedure]
+  (let [from-to-key (str from "->" to)
+        to-from-key (str to "->" from)]
+    (swap! lookup-table-for-unit-conversions assoc from-to-key procedure)
+    (swap! lookup-table-for-unit-conversions assoc to-from-key (unit:invert procedure))))
+
+;;; register the unit conversions
+
+(register-unit-conversion! 'celsius 'kelvin celsius-to-kelvin)
+(register-unit-conversion! 'fahrenheit 'celsius fahrenheit-to-celsius)
+(register-unit-conversion! 'fahrenheit 'kelvin (unit:* fahrenheit-to-celsius celsius-to-kelvin))
+
+(register-unit-conversion! '(div pound (expt inch 2))
+                           '(div newton (expt meter 2))
+                           (unit:div pound-to-newton (unit:expt inch-to-meter 2)))
+
+(register-unit-conversion! '(expt inch 3) '(expt meter 3) (unit:expt inch-to-meter 3))
+
+;;; ----
+
+(defn make-converter
+  [specific implicit]
+  (let [lookup-key (str specific "->" implicit)
+        converter (get @lookup-table-for-unit-conversions lookup-key)]
+    (if converter
+      converter
+      (make-unit-conversion (fn [v1] v1)
+                            (fn [o1] o1)))))
+
+(defn unit-specializer
+  [procedure implicit-output-unit & implicit-input-units]
+  (defn specializer
+    [specific-output-unit & specific-input-units]
+    (let [output-converter (make-converter implicit-output-unit specific-output-unit)
+          input-converts (map make-converter specific-input-units implicit-input-units)]
+      (defn specialized-procedure
+        [& arguments]
+        (output-converter
+         (apply procedure
+                (map (fn [converter argument]
+                       (converter argument))
+                     input-converts
+                     arguments))))
+      specialized-procedure))
+  specializer)
+
+(def make-specialized-gas-law-volume
+  (unit-specializer
+   gas-law-volume
+   '(expt meter 3)
+   '(div newton (expt meter 2))
+   'kelvin
+   'mole))
+
+(def conventional-gas-law-volume
+  (make-specialized-gas-law-volume
+   '(expt inch 3)
+   '(div pound (expt inch 2))
+   'fahrenheit
+   'mole))
+
+
+(t/deftest dsl-final-test
+  (t/is (more-or-less
+         (sphere-radius (conventional-gas-law-volume 14.7 68 1))
+         7.04962459601831)))
+
+(sphere-radius (conventional-gas-law-volume 14.7 68 1))
+;; => 7.04962459601831
